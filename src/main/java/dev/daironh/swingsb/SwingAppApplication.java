baa10de0ba650fa6ab9d3c9177a920957a9a7fb0
Dairon Hernandez <daironh@gmail.com>
2025-02-23 16:29:57 -0600

    Initial commit

---
+++ src/main/java/dev/daironh/swingsb/SwingAppApplication.java
@@ -0,0 +1,13 @@
+package dev.daironh.swingsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/daironh/swingsb/SwingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daironh.swingsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
